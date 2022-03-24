7e0ab2021343be928aad9078563095f7c7dee59d
David Rivera <daleriro@gmail.com>
2022-03-24 16:51:12 -0400

    Solved 08

--- scope-problems-without-context-starter/problems/08-hidden-counter.js
+++ scope-problems-without-context-starter/problems/08-hidden-counter.js
@@ -15,7 +15,20 @@ hidden2(); // returns 1
 
 ***********************************************************************/
 
-// your code here!
+const hiddenCounter = () => {
+  let counter = 0;
+  return (countFunc = () => {
+    counter++;
+    return counter;
+  });
+};
+
+let hidden1 = hiddenCounter(); //returns a function
+console.log(hidden1()); // returns 1
+console.log(hidden1()); // returns 2
+
+let hidden2 = hiddenCounter(); // returns a function
+console.log(hidden2()); // returns 1
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
