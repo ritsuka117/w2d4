b7ee34c2cda15789ceae6c21351059db1f1b2767
David Rivera <daleriro@gmail.com>
2022-03-24 19:11:07 -0400

    Solved 17

--- scope-problems-without-context-starter/problems/17-arrow-reverse.js
+++ scope-problems-without-context-starter/problems/17-arrow-reverse.js
@@ -12,7 +12,7 @@ let result3 = arrowReverseString("potato"); // returns "otatop"
 
 ***********************************************************************/
 
-// your code here!
+const arrowReverseString = (str) => str.split('').reverse().join('');
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 
